eda8b9e9e9957a5ada995ebfbc12d1ea5c783f8c
keyshaagustin34rpl-ctrl <keysha_agustin_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 17:31:47 +0700

    Add files via upload

---
+++ Soal2sedan/src/main/java/com/mycompany/soal2sedan/Soal2sedan.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal2sedan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal2sedan {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("=== PROGRAM MENGHITUNG VOLUME TABUNG ===");
+        System.out.print("Masukkan jari-jari tabung (cm): ");
+        double r = input.nextDouble();
+
+        System.out.print("Masukkan tinggi tabung (cm): ");
+        double t = input.nextDouble();
+
+        double volume = hitungVolumeTabung(r, t);
+        System.out.println("Volume tabung = " + volume + " cm³");
+    }
+    public static double hitungVolumeTabung(double r, double t) {
+        return Math.PI * r * r * t;
+    }
+}
+
+    
+
